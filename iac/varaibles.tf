4961e329cdfc5b38b68563495587ab64eca199fd
Alexander Urbina <w.alexanderuc@gmail.com>
2019-08-16 01:43:19 -0600

    Updated variables ssh_user and ssh_pub_key_file used to connect to GCE Instances

--- iac/varaibles.tf
+++ iac/varaibles.tf
@@ -14,3 +14,11 @@ variable "k8s_image" {
     description = "Boot Disk for K8s Master and Node"
     default = "centos-7-v20190729"
 }
+variable "ssh_user" {
+    description = "SSH User to connect to the instances"
+    default = "ansible"
+}
+variable "ssh_pub_key_file" {
+    description = "SSH Public File to connect to the instances"
+    default = "../keypairs/KUBERNETES-KEYPAIR.pub"
+}
