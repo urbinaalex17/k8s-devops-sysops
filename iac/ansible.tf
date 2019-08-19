8dcd88e634b8e095f274dfe1d92a21233e84a88f
Alexander Urbina <w.alexanderuc@gmail.com>
2019-08-19 00:09:25 -0600

    Updated ansible.tf. New environment block/section instead of exporting the env variable in the command statement

--- iac/ansible.tf
+++ iac/ansible.tf
@@ -4,7 +4,10 @@
 resource "null_resource" "ansible_connection_wait" {
   depends_on = ["google_compute_instance.k8s_master_01"]
   provisioner "local-exec" {
-    command = "export ANSIBLE_CONFIG=${var.ansible_cfg} ansible-playbook -u ${var.ssh_user} ../ansible/playbooks/connection-wait.yml -e target=all"
+    command = "ansible-playbook -u ${var.ssh_user} ../ansible/playbooks/connection-wait.yml -e target=all"
+    environment = {
+      ANSIBLE_CONFIG = "${var.ansible_cfg}"
+    }
   }
 }
 
@@ -12,7 +15,10 @@ resource "null_resource" "ansible_connection_wait" {
 resource "null_resource" "ansible_install_kubernetes" {
   depends_on = ["null_resource.ansible_connection_wait"]
   provisioner "local-exec" {
-    command = "export ANSIBLE_CONFIG=${var.ansible_cfg} ansible-playbook -u ${var.ssh_user} ../ansible/playbooks/install-kubernetes.yml -e target=all"
+    command = "ansible-playbook -u ${var.ssh_user} ../ansible/playbooks/install-kubernetes.yml -e target=all"
+    environment = {
+      ANSIBLE_CONFIG = "${var.ansible_cfg}"
+    }
   }
 }
 
